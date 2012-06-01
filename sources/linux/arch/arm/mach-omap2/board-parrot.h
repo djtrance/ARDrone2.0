e5059a21ff687a1b4f573f9e17a4a5797a240b93
Vincent T.L <vincent.trudel.lapierre@gmail.com>
2012-05-31 22:23:39 -0400

    premier commit

---
+++ sources/linux/arch/arm/mach-omap2/board-parrot.h
@@ -0,0 +1,10 @@
+#include <plat/mmc.h>
+#include <plat/control.h>
+
+
+extern struct omap_mmc_platform_data *hsmmc_data[OMAP34XX_NR_MMC];
+
+void __init board_parrot_init_mmc(struct omap_mmc_platform_data **mmc_data,
+			int nr_controllers);
+
+void __init parrot_omap_init_irq(int rate);
---
+++ sources/linux/lucie/linux.mk
@@ -0,0 +1,31 @@
+#############################################################
+#
+# linux
+#
+#############################################################
+
+ifdef RAPTOR_DIR
+LINUX_DIR:=$(RAPTOR_DIR)/kernel/omap
+else
+LINUX_DIR:=$(PACKAGE_DIR)/omap
+endif
+
+LINUX_EXPORTED_HEADERS := \
+	$(LINUX_DIR)/drivers/parrot/char/pwm/pwm_ioctl.h \
+	$(LINUX_DIR)/drivers/parrot/char/gpio2_ioctl.h \
+	$(LINUX_DIR)/include/linux/media.h \
+	$(LINUX_DIR)/include/linux/videodev2.h \
+	$(LINUX_DIR)/include/linux/v4l2-mediabus.h \
+	$(LINUX_DIR)/include/linux/v4l2-subdev.h 
+
+ifndef LINUXCONF_MK
+	$(error LINUXCONF_MK is not defined)
+endif
+
+include $(LINUXCONF_MK)
+
+#LINUX_MAKE_ARGS:=$(subst $(TARGET_CROSS),/opt/arm-2009q1/bin/arm-none-linux-gnueabi-, $(LINUX_MAKE_ARGS))
+
+LINUX_EXTRA_CMD:=-$(Q) $(MAKE) $(LINUX_MAKE_ARGS) uImage V=$(EXTRA_V); \
+$(Q) cp $(LINUX_DIR_BUILD)/arch/arm/boot/uImage $(BIN_DIR)/
+
